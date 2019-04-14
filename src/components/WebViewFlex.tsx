13d88e7eb9a7704b5acbaf98b4e7f8bb4f051c90
azuy <a.zain212@gmail.com>
2019-04-14 19:58:38 +0500

    added stacknavigator in drawernavigator

--- src/components/WebViewFlex.tsx
+++ src/components/WebViewFlex.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { View, WebView, ViewStyle, StyleProp } from 'react-native';
+import { View, WebView, ViewStyle, StyleProp, NavState, ActivityIndicator, Text } from 'react-native';
 
 interface IProps {
   style: StyleProp<ViewStyle>;
   url: string;
+  onLoadStart?: (event: NavState) => void;
+  onLoadEnd?: (event: NavState) => void;
+  onError?: (event: NavState) => void;
 }
 
 const WebViewFlex = (props: IProps) => {
   return (
     <View style={props.style}>
-      <WebView style={{ flex: 1 }} contentInset={{ right: 50 }} source={{ uri: props.url }} />
+      <WebView
+        style={{ flex: 1 }}
+        contentInset={{ right: 50 }}
+        source={{ uri: props.url }}
+        onLoadStart={props.onLoadStart}
+        onLoadEnd={props.onLoadEnd}
+        // onError={props.onError}
+        renderLoading={ () => (
+          <ActivityIndicator
+            animating={true}
+            color='#84888d'
+            size='large'
+            hidesWhenStopped={true}
+            style={{ alignItems:'center', justifyContent:'center', padding:30, flex: 1 }}
+          />
+        )}
+        startInLoadingState={true}
+        renderError={() => {
+          return <View>
+            <Text>Error</Text>
+          </View>;
+        }}
+      />
     </View>
   );
 };
