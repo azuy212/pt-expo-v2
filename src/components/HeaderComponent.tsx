e4fda6c436022654f68a94b945e7cd5cca04863d
azuy <a.zain212@gmail.com>
2019-05-19 22:58:08 +0500

    Added Search bar

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Left, Button, Icon, Right, Body, Title } from 'native-base';
+import { Header, Left, Button, Icon, Right, Body, Title, Item, Input, Text } from 'native-base';
 import { NavigationScreenProps } from 'react-navigation';
 import { StatusBar, StyleSheet } from 'react-native';
 
@@ -11,13 +11,44 @@ interface IProps {
   title: string;
   isVideoAvailable?: boolean;
   videoIconPress?: () => void;
+  enableSearch?: boolean;
 }
 
 type AllProps = IProps & NavigationScreenProps;
 
-export default class HeaderComponent extends Component<AllProps> {
+interface IState {
+  enableSearch: boolean;
+}
+
+export default class HeaderComponent extends Component<AllProps, IState> {
+  state = {
+    enableSearch: false,
+  };
+
+  enableSearch = () => {
+    this.setState({ enableSearch: true });
+  }
+
+  disableSearch = () => {
+    this.setState({ enableSearch: false });
+  }
+
   render() {
-    return (
+    return this.state.enableSearch ? (
+      <Header
+        style={{ height: headerHeight + statusBarHeight, paddingTop: statusBarHeight }}
+        searchBar={true}
+        rounded={true}>
+        <Item>
+          <Icon name='ios-search' />
+          <Input placeholder='Search' />
+          <Icon name='md-close-circle' onPress={this.disableSearch} />
+        </Item>
+        <Button transparent={true}>
+          <Text>Search</Text>
+        </Button>
+      </Header>
+    ) : (
       <Header style={{ height: headerHeight + statusBarHeight }}>
         <Left style={styles.header}>
           <Button transparent={true} onPress={() => this.props.navigation.openDrawer()}>
@@ -27,18 +58,14 @@ export default class HeaderComponent extends Component<AllProps> {
         <Body style={styles.headerTitle}>
           <Title>{this.props.title}</Title>
         </Body>
-        {this.props.isVideoAvailable ? (
-          <Right style={styles.header}>
-            <Icon
-              name='play-video'
-              type='Foundation'
-              style={styles.iconRight}
-              onPress={this.props.videoIconPress}
-            />
-          </Right>
-        ) : (
-          <Right />
-        )}
+        <Right style={styles.header}>
+          <Icon
+            name={this.props.isVideoAvailable ? 'play-circle-outline' : 'search'}
+            type='MaterialIcons'
+            style={styles.iconRight}
+            onPress={this.props.isVideoAvailable ? this.props.videoIconPress : this.enableSearch}
+          />
+        </Right>
       </Header>
     );
   }
