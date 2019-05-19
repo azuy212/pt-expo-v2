5f71e291cb7dbf60181eb0fcacc14fc1e5475d92
azuy <a.zain212@gmail.com>
2019-05-19 11:42:22 +0500

    Added logo in Quiz

--- src/screens/QuestionQuizzes/Quizzes/QuizComplete.tsx
+++ src/screens/QuestionQuizzes/Quizzes/QuizComplete.tsx
@@ -3,23 +3,24 @@ import { StyleSheet } from 'react-native';
 import { H1, H3, Container, Content } from 'native-base';
 import HeaderComponent from '../../../components/HeaderComponent';
 import { NavigationScreenProps } from 'react-navigation';
+import { calculatePercentage } from '../../../services/common';
 
-export default class QuizComplete extends Component<NavigationScreenProps> {
-  render() {
-    const { correctAnswers, totalQuizzes } = this.props.navigation.state.params!;
-    return (
-      <Container>
-        <HeaderComponent title='Quiz Result' {...this.props} />
+const QuizComplete = (props: NavigationScreenProps) => {
+  const { correctAnswers, totalQuizzes } = props.navigation.state.params!;
+  return (
+    <Container>
+        <HeaderComponent title='Quiz Result' {...props} />
           <H1 style={{ margin: 20 }}>You have completed the quiz!</H1>
         <Content contentContainerStyle={styles.container}>
           <H3>Score: {correctAnswers}</H3>
           <H3>Total: {totalQuizzes}</H3>
-          <H3>Percentage: {(correctAnswers / totalQuizzes) * 100}%</H3>
+          <H3>Percentage: {calculatePercentage(correctAnswers, totalQuizzes)}%</H3>
         </Content>
       </Container>
-    );
-  }
-}
+  );
+};
+
+export default QuizComplete;
 
 const styles = StyleSheet.create({
   container: {
