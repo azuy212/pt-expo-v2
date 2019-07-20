d02a0019127f668dae630e4dda007d01b1b10ca7
azuy <a.zain212@gmail.com>
2019-07-20 23:51:49 +0500

    Some Fixes in Quiz Detail and Quiz Result Screen

--- src/services/error.ts
+++ src/services/error.ts
@@ -1,7 +1,7 @@
-import { Alert } from 'react-native';
+import { ToastAndroid } from 'react-native';
 
 export function showErrorAlert(title: string, err: any) {
   const error = !err.message ? err : err.message;
   console.log(title, error);
-  Alert.alert(title, error);
+  ToastAndroid.showWithGravity(error, ToastAndroid.SHORT, ToastAndroid.CENTER);
 }
