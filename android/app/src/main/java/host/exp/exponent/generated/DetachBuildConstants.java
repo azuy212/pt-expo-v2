9674f912b17da2bcfd607761e454d13e162dd1f9
azuy <a.zain212@gmail.com>
2019-07-22 23:56:45 +0500

    App detached to ExpoKit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp43d808f1c9f94d94be47a45e1b11a850://192.168.0.106:19000";
+
+}
